e1529c62efca233847ae1a3838e8080f6e278a15
Sandrina <sjtrela@gmail.com>
2016-11-11 17:08:26 +0100

    changed js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@
 var name = prompt('Wpisz swoje imię');
 alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+// triangle area script
+
+var a = prompt('Podaj długość podstawy trójkąta'),
+	h = prompt('Podaj wysokość trójkąta'),
+	triangleArea = a*h/2;
+alert('Pole trójkąta o podstawie: ' + a + ' i wysokości: ' + h + ' wynosi: ' + triangleArea)
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
