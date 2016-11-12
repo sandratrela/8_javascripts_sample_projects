4b94b4f26f4aa1a4566288c322110082b626421f
Sandrina <sjtrela@gmail.com>
2016-11-12 11:46:18 +0100

    new JS value script add

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,23 @@ var a = prompt('Podaj długość podstawy trójkąta'),
 	triangleArea = a*h/2;
 alert('Pole trójkąta o podstawie: ' + a + ' i wysokości: ' + h + ' wynosi: ' + triangleArea)
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+// value plus-or-minus script
+
+var a = prompt('podaj wartość a'),
+    b = prompt('podaj wartość b'),
+    value = (a * a) + (2 * a * b) - (b * b);
+console.log('Wynik to ' + value);
+
+if (value > 0) {
+  console.log('Wynik jest dodatni');
+  alert('Wynik jest dodatni');
+
+} else if (value < 0) {
+  console.log('Wynik jest ujemny');
+  alert('Wynik jest ujemny');
+
+} else {
+  console.log('Zero');
+  alert('Zero');
+}
